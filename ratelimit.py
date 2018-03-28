83ff0155d1756314d84e519e14a9c36fed95f81f
Nizar Lazuardy Firmansyah <n.lazuardy@gmail.com>
2018-03-29 06:14:18 +0700

    staging #comment fix per params

--- ratelimit.py
+++ ratelimit.py
@@ -41,7 +41,7 @@ def on_over_limit(limit):
     return json.dumps({'status':'Fail',
         'reason':'You can only use the faucet 1 XTL/day'}),429
 
-def ratelimit(limit, per=100, send_x_headers=True,
+def ratelimit(limit, per=200, send_x_headers=True,
               over_limit=on_over_limit,
               fp_func=lambda: request.form.get('fingerprint'),
               ip_func=lambda: request.environ['REMOTE_ADDR'],
